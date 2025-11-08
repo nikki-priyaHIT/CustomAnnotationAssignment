e0faf009e7c28721fbc5bc3504cb23f4c28a01a4
Anshu Priya <anshupriya1278@gmail.com>
2025-11-08 15:50:19 +0530

    Commit

--- springAssignment/src/main/java/com/springProject/springAssignment/dbInnitializer/DataBaseInitialize.java
+++ springAssignment/src/main/java/com/springProject/springAssignment/dbInnitializer/DataBaseInitialize.java
@@ -10,16 +10,13 @@ public class DataBaseInitialize {
 
     private final RepoLayer repo;
 
-    // Constructor injection
     public DataBaseInitialize(RepoLayer repo) {
         this.repo = repo;
     }
 
     @PostConstruct
     public void preload() {
-       // System.out.println("Database initialized");
 
-        // Insert data only if DB is empty
         if (repo.count() == 0) {
             repo.save(new Entities("welcome", "Welcome, this is a custom annotation project."));
             repo.save(new Entities("bye", "Bye, see you again next time."));
